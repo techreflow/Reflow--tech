465fc521c44427e15b484a2a3492ad8382854986
Aditya Coomar <adi.coomar04@gmail.com>
2026-01-07 13:33:39 +0530

    fix: update links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from './components/Hero';
-import Services from './components/Services';
-import OurProduct from './components/OurProduct';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import OurProduct from "./components/OurProduct";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
